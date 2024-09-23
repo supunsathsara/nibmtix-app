d1acf2462cb9c3da574a332a8395d154e4c94b6f
Savindu Supun <contact@supunsathsara.com>
2024-09-24 00:30:07 +0530

    Refactor AndroidManifest.xml to add camera and audio permissions

--- utils/supabase.js
+++ utils/supabase.js
@@ -2,8 +2,9 @@ import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = "https://mwsjezpvwhcatnyrumey.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im13c2plenB2d2hjYXRueXJ1bWV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI5NjY1NTksImV4cCI6MjAzODU0MjU1OX0.wDmh-klhDbcYg30dPX6EXWRiYAZ5uFFaAJ_tf_UlGqc";
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
